2a346b756978f21ae533a76a39c70539e05f6760
Dima Afanasev <duma12let@gmail.com>
2020-10-01 12:53:03 +0300

    Add index view, permanent redirect to news app and namespace "news"

--- news_engine/urls.py
+++ news_engine/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+from .views import news_redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', include('news.urls')),
+    path('', news_redirect),
 ]
---
+++ news_engine/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import redirect
+
+
+def news_redirect(request):
+    return redirect('news:index', permanent=True)
