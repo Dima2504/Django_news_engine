5e766605a8000244da73e67eaf1f66eb655c28bf
Dima Afanasev <duma12let@gmail.com>
2020-10-11 15:26:50 +0300

    Add Category model and register it

--- news/models.py
+++ news/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+from django.shortcuts import reverse
+from django.utils.text import slugify
+
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField('Категорія', max_length=100, help_text='Категорія для новин')
+    description = models.TextField('Опис', blank=True, help_text='Короткий опис категорії')
+    slug = models.SlugField(max_length=160, unique=True,
+                            help_text='Слаг для url, генерується сам із назви, але можна задат вручну',
+                            blank=True)
+
+    class Meta:
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.name and not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
