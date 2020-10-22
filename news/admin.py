6d4248c2dbb73fe910c133182a6148bf413e7bba
Dima Afanasev <duma12let@gmail.com>
2020-10-22 16:08:11 +0300

    Add news model and migration

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Category
+from .models import News
 # Register your models here.
 
 admin.site.register(Category)
+admin.site.register(News)
--- news/models.py
+++ news/models.py
@@ -25,3 +25,24 @@ class Category(models.Model):
         if self.name and not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+
+
+class News(models.Model):
+    category = models.ForeignKey(Category, related_name='news', on_delete=models.CASCADE)
+    author = models.CharField(max_length=130, verbose_name='Автор')
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Опис', help_text='Короткий фрагмент')
+    url = models.URLField(verbose_name='Посилання на ресурс')
+    url_to_image = models.URLField(verbose_name='Посилання на картинку')
+    published_at = models.DateTimeField('Дата публікації')
+    content = models.TextField(verbose_name='Контент')
+
+    class Meta:
+        verbose_name = 'Новина'
+        verbose_name_plural = 'Новини'
+        ordering = ['published_at',]
+
+    def __repr__(self):
+        return self.title
+    def __str__(self):
+        return self.title
