57b8343a337d12be9b63bc5f134a4a62e18079d9
Dima Afanasev <duma12let@gmail.com>
2020-11-23 15:59:01 +0200

    Add some small api functions to get News and Users

--- news/api_views.py
+++ news/api_views.py
@@ -1,9 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.status import HTTP_202_ACCEPTED, HTTP_404_NOT_FOUND
 from .models import News
-from .serializers import NewsListSerializer
+from .serializers import CategorySerializer, UserSerializer, NewsSerializer, \
+    TelegramUserSerializer
 
 from django.shortcuts import get_list_or_404
+
+from auth_system.models import User
+
+from allauth.socialaccount.models import SocialAccount
 
 
 class NewsListView(APIView):
@@ -14,5 +20,39 @@ class NewsListView(APIView):
             news = News.objects.all()
         if 'limit' in request.query_params:
             news = news[:int(request.query_params.get('limit'))]
-        serializer = NewsListSerializer(news, many=True)
+        serializer = NewsSerializer(news, many=True)
         return Response(serializer.data)
+
+class NewsDetailView(APIView):
+    def get(self, request, slug):
+        try:
+            news = News.objects.get(slug=slug)
+            serializer = NewsSerializer(news)
+            return Response(serializer.data)
+        except News.DoesNotExist as e:
+            return Response(status=HTTP_404_NOT_FOUND, exception=e,
+                            data={'details': 'News with such slug does not exist'})
+
+
+class UsersListView(APIView):
+    def get(self, request):
+        serializer = UserSerializer(User.objects.exclude(is_staff=True), many=True)
+        return Response(serializer.data)
+
+
+class TelegramUsersListView(APIView):
+    def get(self, request):
+        serializer = TelegramUserSerializer(
+            SocialAccount.objects.filter(provider='custom_telegram'), many=True)
+        return Response(serializer.data)
+
+
+class UserDetailView(APIView):
+    def get(self, request, telegram_id):
+        try:
+            user = SocialAccount.objects.get(uid=telegram_id).user
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        except SocialAccount.DoesNotExist as e:
+            return Response(status=HTTP_404_NOT_FOUND, exception=e,
+                            data={'details': 'User with such telegram id does not exist'})
--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from .views import *
-from .api_views import NewsListView
+import news.api_views as api
 
 app_name = 'news'
 
 urlpatterns = [
     path('', AllNews.as_view(), name='start'),
-    path('api/news-list/', NewsListView.as_view(), name='news-list'),  # TODO need to be changed
+    path('api/news/', api.NewsListView.as_view(), name='api_news_list'),
+    path('api/news/<slug:slug>/', api.NewsDetailView.as_view(), name='api_news_detail'),
+    path('api/users/', api.UsersListView.as_view(), name='api_users_list'),
+    path('api/telegram-users/', api.TelegramUsersListView.as_view(), name='api_telegram_users_list'),
+    path('api/telegram-users/<int:telegram_id>/', api.UserDetailView.as_view(), name='api_user_detail'),
 
     path('personal-account/', PersonalAccount.as_view(), name='personal_account'),
     path('news-history/', NewsHistory.as_view(), name='news_history'),
